4bd907c83616879400fe26b698aa142d436b3092
Atefeh_MN <atefeh.nasrabadi1993@gmail.com>
2021-12-14 15:17:22 +0330

    start new project to learn redux

---
+++ src/components/CakesContianer.jsx
@@ -0,0 +1,21 @@
+import { buyCake } from "../redux/cakeRedux/CakeActions";
+import { connect } from "react-redux";
+
+const CakesContianer = (props) => {
+    return (<div>
+        <h2>number of cakes-{ props.numOfCakes}</h2>
+        <button onClick={props.buyCake}>Buy cake</button>
+    </div> );
+}
+
+const mapStateToProps = (state) => {
+    return {
+        numOfCakes: state.numOfCakes,
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        buyCake: () => dispatch(buyCake()),
+    }
+} 
+export default connect(mapStateToProps,mapDispatchToProps)(CakesContianer);
---
+++ src/redux/cakeRedux/CakeActions.js
@@ -0,0 +1,6 @@
+import { BUY_CAKES } from "./CakeType";
+
+export function buyCake () {
+    return { type: BUY_CAKES };
+};
+
